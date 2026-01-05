83fa5c302db4debf516c1b91221c2a62d1163051
IzumiDKC <huudien111@gmail.com>
2026-01-06 01:38:02 +0700

    Add route details to post details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 import { HomeComponent } from '../home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import { PostDetailComponent } from './post-detail/post-detail.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   
@@ -24,6 +24,11 @@ export const routes: Routes = [
     canActivate: [authGuard] 
   },
 
+  { 
+    path: 'posts/:id', 
+    component: PostDetailComponent,
+    canActivate: [authGuard] // Nên thêm guard nếu muốn bảo mật
+  },
 
   { path: '**', redirectTo: 'home' }
 ];
